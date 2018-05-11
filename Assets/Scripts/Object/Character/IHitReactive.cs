b1440ad5fd822795a6bf83a520d33e70de74148b
ParkToKe <34452518+ParkToKe@users.noreply.github.com>
2018-05-12 04:54:21 +0900

    0511_라이팅
    복도 라이팅 수정

--- Assets/Scripts/Object/Character/IHitReactive.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-public interface IHitReactive : IDamageable
-{
-    void ReactToHit(Collider collider, Vector3 point, Vector3 force);
-}
--- Assets/Scripts/TimeControl/ITimeControl.cs
+++
@@ -1,5 +0,0 @@
-﻿
-public interface ITimeControl
-{
-    void AdjustTimeScale(float ratio);
-}
